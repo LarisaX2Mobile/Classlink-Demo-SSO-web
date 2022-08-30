57559f2ffd2f66ceab39df0f105ff13ebfe4aa2e
Larisa Mihut <larisamihut26@gmail.com>
2022-08-30 19:23:46 +0300

    First commit

---
+++ src/test/java/classlink/demo/ssoapp/SsoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package classlink.demo.ssoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
